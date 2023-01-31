0b2ec1c9549e6fec3705797d4231800103c4b4a6
Longtao Zhang <DragonBillow@outlook.com>
2023-01-31 10:33:49 +0800

    feat(cmake): cmake 支持选择 Qt5 和 Qt6
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- cmake/FindDeployQt.cmake
+++ cmake/FindDeployQt.cmake
@@ -1,6 +1,6 @@
-find_package(Qt5 COMPONENTS Core)
+find_package(Qt${QT_DEFAULT_MAJOR_VERSION} COMPONENTS Core)
 find_program(
   DeployQt
   NAMES windeployqt macdeployqt
-  PATHS "${CMAKE_PREFIX_PATH}/bin" "${_qmake_dir}" "${Qt5Core_DIR}/../../../bin"
+  PATHS "${CMAKE_PREFIX_PATH}/bin" "${_qmake_dir}" "${Qt${QT_DEFAULT_MAJOR_VERSION}Core_DIR}/../../../bin"
 )

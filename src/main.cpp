ae60bed769996701cd32f5ae89ad6b540b17f25d
Longtao Zhang <DragonBillow@outlook.com>
2023-01-31 14:04:05 +0800

    feat(tesseract): 设置 TESSDATA_PREFIX 变量
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,21 @@
 #include <spdlog/spdlog.h>
 
 #include <QApplication>
+#include <QtGlobal>
 
 #include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
+void SetEnvIfNotExists(const char* name, QString const& val) {
+    auto res = qgetenv(name);
+    if(!res.isEmpty()) return;
+
+    qputenv(name, val.toLocal8Bit());
+}
+
+int main(int argc, char* argv[]) {
+    auto cur       = QApplication::applicationDirPath();
+    auto data_path = cur + "tessdata";
+    SetEnvIfNotExists("TESSDATA_PREFIX", data_path);
     QApplication app(argc, argv);
 
     MainWindow w;
